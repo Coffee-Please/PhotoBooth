e93e85468566f4fe7eeb0f3e5c2f04b5d1febb4d
Coffee-Please <priya.singh.310@my.csun.edu>
2021-05-19 11:27:44 -0700

    Adding comments to delete modal

--- src/components/confirmDeleteModal.js
+++ src/components/confirmDeleteModal.js
@@ -22,15 +22,17 @@ Modal.setAppElement('#root'); // for accessibility, prevents screen readers from
 const ConfirmDeleteModal = ({ method, type, albumName, field, userId, selectedItem, setSelectedItem, selectedAlbum, setSelectedAlbum }) => {
   const [modalIsOpen,setIsOpen] = useState(false); // holds modal status
 
-
+  // function to open the modal
   function openModal() {
     setIsOpen(true);
   }
 
+  // function to close the modal
   function closeModal(){
     setIsOpen(false);
   }
 
+  // function to handle the sumit and start the deletion of the image or album
   const handleSubmit = () => {
     ManageItems(method, albumName, field, userId, selectedItem, setSelectedItem, selectedAlbum, setSelectedAlbum);
   }
@@ -38,8 +40,10 @@ const ConfirmDeleteModal = ({ method, type, albumName, field, userId, selectedIt
   return (
     // modal
     <>
+        {/* Delete Button */}
         <button className="ui red button" id="delete-btn" alt="Delete" onClick={ openModal }><CgTrash /></button>
 
+        {/* Delete Modal */}
         <Modal
           isOpen={modalIsOpen}
           onRequestClose={closeModal}
@@ -47,12 +51,15 @@ const ConfirmDeleteModal = ({ method, type, albumName, field, userId, selectedIt
           contentLabel="Delete Image"
         >
 
+          {/* Delete Modal Header */}
           <h2>Delete</h2>
 
+          {/* Delete Modal Form*/}
           <form>
             <p>Are you sure you want to delete this {type}?</p>
           </form>
 
+          {/* Delete Modal Buttons */}
           <button className="ui basic button" onClick={closeModal}>Cancel</button>
           <button className="ui red button" onClick={handleSubmit}>Delete</button>
         </Modal>
